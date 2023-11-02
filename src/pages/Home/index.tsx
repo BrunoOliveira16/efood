4f26fd7f1bd604207085cb8fa022e79dfaa5558d
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-02 19:12:20 -0300

    :sparkles: feat: criado home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Hero from '../../components/Hero'
+import { Container } from '../../global/globalStyle'
+
+const Home = () => (
+  <div>
+    <Hero />
+    <Container></Container>
+  </div>
+)
+
+export default Home
