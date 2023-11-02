a28dd30aaf422c2664edc28a4fb0d066debb8f57
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-11-02 19:11:57 -0300

    :sparkles: feat: criado hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+import Logo from '../Logo'
+import { HeroContainer, Title } from './styles'
+
+const Hero = () => (
+  <HeroContainer>
+    <Logo />
+    <div>
+      <Title>Viva experiências gastronômicas no conforto da sua casa</Title>
+    </div>
+  </HeroContainer>
+)
+
+export default Hero
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+import theme from '../../global/theme'
+import backgroundImage from '../../assets/hero.png'
+
+export const HeroContainer = styled.header`
+  width: 100%;
+  padding: 64px 0 40px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: url(${backgroundImage});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  div {
+    margin: 0 auto;
+  }
+`
+
+export const Title = styled.h1`
+  max-width: 539px;
+  color: ${theme.Colors.text};
+  text-align: center;
+  font-size: ${theme.fontSizes.headingXl};
+  font-weight: 900;
+  line-height: normal;
+  margin-top: 138px;
+`
