5e677e884316dd6edf431529d91968cab3c1f3be
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-02 16:50:57 -0300

    feat: criado novos componentes estilizados e props para Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,18 +1,25 @@
+import { Container } from '../../global/globalStyle'
+import { HeroContainer, HeroHeader, Text, TextContainer, Title } from './styles'
 import Logo from '../Logo'
-import { HeroContainer, Title, TitleContainer } from './styles'
 
 type HeroProps = {
   title?: string
+  textContent?: string
 }
 
-const Hero = ({ title }: HeroProps) => (
+const Hero = ({ textContent, title }: HeroProps) => (
   <HeroContainer>
-    <Logo />
-    {title && (
-      <TitleContainer>
-        <Title>{title}</Title>
-      </TitleContainer>
-    )}
+    <Container>
+      <HeroHeader>
+        {title && <Title>{title}</Title>}
+        <Logo />
+      </HeroHeader>
+      {textContent && (
+        <TextContainer>
+          <Text>{textContent}</Text>
+        </TextContainer>
+      )}
+    </Container>
   </HeroContainer>
 )
 
--- src/components/Hero/styles.ts
+++ src/components/Hero/styles.ts
@@ -5,7 +5,6 @@ import backgroundImage from '../../assets/hero.png'
 export const HeroContainer = styled.header`
   width: 100%;
   padding: 64px 0 40px;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -14,11 +13,25 @@ export const HeroContainer = styled.header`
   background-repeat: no-repeat;
 `
 
-export const TitleContainer = styled.div`
-  margin: 0 auto;
+export const HeroHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `
 
 export const Title = styled.h1`
+  color: ${theme.Colors.text};
+  text-align: center;
+  font-size: ${theme.fontSizes.headingM};
+  font-weight: 900;
+`
+
+export const TextContainer = styled.div`
+  margin: 0 auto;
+`
+
+export const Text = styled.h2`
   max-width: 539px;
   color: ${theme.Colors.text};
   text-align: center;
