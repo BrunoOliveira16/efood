40a2fe52005cdda3c91dc4c4249d7b43101a3435
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-02 16:51:27 -0300

    feat: ajuste de grid layout em CardListHome

--- src/containers/CardListHome/styles.ts
+++ src/containers/CardListHome/styles.ts
@@ -5,7 +5,7 @@ export const CardListContainer = styled.div`
   padding: 80px 0 120px;
   margin: 0 auto;
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   column-gap: 80px;
   row-gap: 48px;
 
