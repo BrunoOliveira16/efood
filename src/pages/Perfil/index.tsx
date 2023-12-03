c1b512c8ee9b4450cab32cd8f4fa3ca23b659836
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-03 10:53:07 -0300

    feat: refatoração em Perfil

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,22 +1,12 @@
 import Hero from '../../components/Hero'
-import BannerImage from '../../assets/banner-img.png'
-import { BannerContainer, PerfilContainer, SubTitle, Title } from './styles'
-import { Container } from '../../global/globalStyle'
+import { PerfilContainer } from './styles'
 import CardListPerfil from '../../containers/CardListPerfil'
 
 const Perfil = () => {
   return (
     <PerfilContainer>
       <Hero title="Restaurantes" />
-      <BannerContainer style={{ backgroundImage: `url(${BannerImage})` }}>
-        <Container>
-          <SubTitle>Italiana</SubTitle>
-          <Title>La Dolce Vita Trattoria</Title>
-        </Container>
-      </BannerContainer>
-      <Container>
-        <CardListPerfil />
-      </Container>
+      <CardListPerfil />
     </PerfilContainer>
   )
 }
