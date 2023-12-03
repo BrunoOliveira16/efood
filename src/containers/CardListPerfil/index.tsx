7809e05c24428acd5dd7c5f13c0e1fad1be4ec71
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-03 07:43:43 -0300

    feat: criado CardListPerfil

---
+++ src/containers/CardListPerfil/index.tsx
@@ -0,0 +1,69 @@
+import Card from '../../components/Card'
+import CoverImage from '../../assets/pizza-img.png'
+import { CardListContainer } from './styles'
+
+const products = [
+  {
+    id: 1,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  },
+  {
+    id: 2,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  },
+  {
+    id: 3,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  },
+  {
+    id: 4,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  },
+  {
+    id: 5,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  },
+  {
+    id: 6,
+    title: 'Pizza Marguerita',
+    description:
+      'A clássica Marguerita: molho de tomate suculento, mussarela derretida, manjericão fresco e um toque de azeite. Sabor e simplicidade!',
+    cover: CoverImage
+  }
+]
+
+const CardListPerfil = () => {
+  return (
+    <CardListContainer>
+      {products.map((item) => (
+        <>
+          <Card
+            key={item.id}
+            card="second"
+            nameButton="Adicionar ao carrinho"
+            title={item.title}
+            description={item.description}
+            cover={item.cover}
+          />
+        </>
+      ))}
+    </CardListContainer>
+  )
+}
+
+export default CardListPerfil
