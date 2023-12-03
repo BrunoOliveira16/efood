ab403e45b9a6408af2e4370427c870a1aeddc646
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-03 10:53:34 -0300

    feat: refatoração em CardListHome

--- src/containers/CardListHome/index.tsx
+++ src/containers/CardListHome/index.tsx
@@ -1,74 +1,29 @@
 import Card from '../../components/Card'
-import CoverImage from '../../assets/image-01.png'
-import Star from '../../assets/icons/star.svg'
-import { CardListContainer } from './styles'
+import { restaurantsData } from '../../utils'
 
-const restaurants = [
-  {
-    id: 1,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  },
-  {
-    id: 2,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  },
-  {
-    id: 3,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  },
-  {
-    id: 4,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  },
-  {
-    id: 5,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  },
-  {
-    id: 6,
-    title: 'Hioki Sushi',
-    rating: '4.9',
-    tagName: 'Japonesa',
-    description:
-      'Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão sem sair do lar com nosso delivery!',
-    cover: CoverImage
-  }
-]
+import { CardListContainer } from './styles'
+import Star from '../../assets/icons/star.svg'
+
+type RestaurantsDataProps = {
+  id: number
+  title: string
+  rating: string
+  tagName: string[]
+  description: string
+  cover: string
+}
 
 const CardListHome = () => {
   return (
     <CardListContainer>
-      {restaurants.map((item) => (
+      {restaurantsData.map((item: RestaurantsDataProps) => (
         <>
           <Card
             key={item.id}
             card="primary"
+            kindButton="link"
             nameButton="Saiba Mais"
+            to="/perfil"
             iconName={Star}
             title={item.title}
             description={item.description}
