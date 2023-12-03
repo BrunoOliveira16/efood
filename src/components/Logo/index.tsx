fd956b325074e271886ddd982bc87f679b001d08
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-03 10:50:36 -0300

    feat: adicionado nova variação de tipo para Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,10 +1,26 @@
-import LogoImage from '../../assets/logo.png'
-import { LogoContainer } from './styles'
+import { LogoContainer, LogoContainerLink } from './styles'
 
-const Logo = () => (
-  <LogoContainer>
-    <img src={LogoImage} alt="logo eFood" />
-  </LogoContainer>
-)
+type LogoProps = {
+  kind: 'image' | 'link'
+  title: string
+  cover: string
+  to?: string
+}
+
+const Logo = ({ kind, title, cover, to }: LogoProps) => {
+  if (kind === 'link') {
+    return (
+      <LogoContainerLink to={to as string}>
+        <img src={cover} alt={title} />
+      </LogoContainerLink>
+    )
+  }
+
+  return (
+    <LogoContainer>
+      <img src={cover} alt={title} />
+    </LogoContainer>
+  )
+}
 
 export default Logo
--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const LogoContainer = styled.div`
   margin: 0 auto;
+
+  img {
+    width: 125px;
+    height: 57px;
+  }
+`
+export const LogoContainerLink = styled(Link)`
+  margin: 0 auto;
+  text-decoration: none;
 
   img {
     width: 125px;
