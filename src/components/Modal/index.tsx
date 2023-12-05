676a4dc59398ac4e8b2fbcfeb3e773551e41cda5
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-05 11:16:21 -0300

    feat: alterado logica e estilização do modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,14 @@
-import { ModalContainer, ContainerImage, Title, Text } from './styles'
-import { Container } from '../../global/globalStyle'
 import Button from '../Button'
+import {
+  ModalContainer,
+  ContainerImage,
+  Title,
+  Text,
+  Fade,
+  ContainerText
+} from './styles'
+import { Container } from '../../global/globalStyle'
+import { MdClose } from 'react-icons/md'
 
 type ModalProps = {
   title: string
@@ -8,28 +16,39 @@ type ModalProps = {
   description: string
   potion: string
   price: number
+  closeModal: () => void
 }
 
-const Modal = ({ cover, title, description, potion, price }: ModalProps) => {
+const Modal = ({
+  cover,
+  title,
+  description,
+  potion,
+  price,
+  closeModal
+}: ModalProps) => {
   return (
-    <Container>
-      <ModalContainer>
-        <ContainerImage>
-          <img src={cover} alt={title} />
-        </ContainerImage>
-        <div>
-          <Title>{title}</Title>
-          <Text>{description}</Text>
-          <Text>Serve: de {potion}</Text>
-          <Button
-            kind="button"
-            displayMode="inlineBlock"
-            themeMode="second"
-            placeholder={`Adicionar ao carrinho - R$ ${price}`}
-          />
-        </div>
-      </ModalContainer>
-    </Container>
+    <Fade>
+      <Container>
+        <ModalContainer>
+          <ContainerImage>
+            <img src={cover} alt={title} />
+          </ContainerImage>
+          <ContainerText>
+            <Title>{title}</Title>
+            <Text>{description}</Text>
+            <Text>Serve: de {potion}</Text>
+            <Button
+              kind="button"
+              displayMode="inlineBlock"
+              themeMode="second"
+              placeholder={`Adicionar ao carrinho - R$ ${price}`}
+            />
+          </ContainerText>
+          <MdClose onClick={closeModal} />
+        </ModalContainer>
+      </Container>
+    </Fade>
   )
 }
 
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,33 +1,64 @@
 import styled from 'styled-components'
 import theme from '../../global/theme'
+import { Container } from '../../global/globalStyle'
+
+export const Fade = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background-color: ${theme.Colors.overlayColor};
+  z-index: 2;
+
+  ${Container} {
+    position: relative;
+  }
+`
 
 export const ModalContainer = styled.div`
   display: flex;
   gap: 24px;
   padding: 32px;
+  margin-top: 300px;
   background-color: ${theme.Colors.text};
   color: ${theme.Colors.white};
+  position: relative;
+
+  svg {
+    width: 18px;
+    height: 18px;
+    position: absolute;
+    top: 8px;
+    right: 8px;
+    cursor: pointer;
+  }
 `
 
 export const ContainerImage = styled.div`
-  width: 280px;
-  height: 280px;
-  flex-shrink: 0;
+  max-width: 280px;
+  width: 100%;
 
   img {
     width: 100%;
-    object-fir: cover;
+    height: 280px;
+    object-fit: cover;
   }
+`
+
+export const ContainerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `
 
 export const Title = styled.h2`
   font-size: ${theme.fontSizes.headingM};
   font-weight: 900;
+  margin-bottom: 16px;
 `
 
 export const Text = styled.p`
   font-size: ${theme.fontSizes.bodyS};
   font-weight: 400;
   line-height: 22px;
-  margin-top: 16px;
+  margin-bottom: 18px;
 `
