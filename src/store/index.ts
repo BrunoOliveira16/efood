78a56867cf32b8d06938423d3b458de19e80ce77
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-14 00:25:11 -0300

    feat: criado e configurado store

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import api from '../services/api'
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware)
+})
+
+export type RootReducer = ReturnType<typeof store.getState>
