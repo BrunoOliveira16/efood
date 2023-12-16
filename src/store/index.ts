1c52e5e15ebc7567a797992c64a582ceb1c8911a
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-16 18:25:19 -0300

    feat: adicionado reducer cart

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import api from '../services/api'
+import cartReducer from './reducers/cart'
 
 export const store = configureStore({
   reducer: {
+    cart: cartReducer,
     [api.reducerPath]: api.reducer
   },
   middleware: (getDefaultMiddleware) =>
