d3a1676153cf260926d13091d661d8afb4d63086
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-16 18:27:23 -0300

    feat: criado reducer cart

---
+++ src/store/reducers/cart.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { MenuDataProps } from '../../containers/CardListHome'
+
+type CartState = {
+  items: MenuDataProps[]
+  isOpen: boolean
+}
+
+const initialState: CartState = {
+  items: [],
+  isOpen: false
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<MenuDataProps>) => {
+      state.items.find((item) => item.id === action.payload?.id)
+      state.items.push(action.payload)
+    },
+    remove: (state, action: PayloadAction<number>) => {
+      state.items = state.items.filter((item) => item.id !== action.payload)
+    },
+    open: (state) => {
+      state.isOpen = true
+    },
+    close: (state) => {
+      state.isOpen = false
+    }
+  }
+})
+
+export const { add, remove, open, close } = cartSlice.actions
+export default cartSlice.reducer
