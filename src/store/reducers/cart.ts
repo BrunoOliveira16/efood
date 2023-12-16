f468afaa6ebd5dc5d3a96ce54b5225ad9c356c2e
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2023-12-16 19:12:18 -0300

    feat: criado reducer cart

--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -16,8 +16,13 @@ const cartSlice = createSlice({
   initialState,
   reducers: {
     add: (state, action: PayloadAction<MenuDataProps>) => {
-      state.items.find((item) => item.id === action.payload?.id)
-      state.items.push(action.payload)
+      const product = state.items.find((item) => item.id === action.payload?.id)
+
+      if (!product) {
+        state.items.push(action.payload)
+      } else {
+        alert('O produto já está no carrinho')
+      }
     },
     remove: (state, action: PayloadAction<number>) => {
       state.items = state.items.filter((item) => item.id !== action.payload)
