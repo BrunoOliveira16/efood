54351b34b1999586b049ae1b0c2e0bd92a2c70d7
Bruno Oliveira <bruno1_oliveira@hotmail.com>
2024-01-06 19:18:21 -0300

    style: adicionado novos componentes estilizados para Cart

---
+++ src/containers/Cart/styles.ts
@@ -0,0 +1,149 @@
+import styled from 'styled-components'
+import theme from '../../global/theme'
+import { ButtonContainer } from '../../components/Button/styles'
+
+type CartContainerProps = {
+  isOpen: boolean
+}
+
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+export const CartContainer = styled.div<CartContainerProps>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: flex-end;
+  z-index: 3;
+  display: ${(props) => (props.isOpen === true ? 'flex' : 'none')};
+`
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${theme.Colors.overlayColor};
+`
+
+export const Sidebar = styled.aside`
+  max-width: 360px;
+  width: 100%;
+  padding: 32px 8px;
+  background-color: ${theme.Colors.text};
+  z-index: 3;
+`
+
+export const CartItem = styled.li`
+  display: flex;
+  padding: 12px 8px;
+  margin-bottom: 16px;
+  position: relative;
+  background-color: ${theme.Colors.primary};
+
+  img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    margin-right: 8px;
+  }
+
+  svg {
+    width: 16px;
+    height: 16px;
+    position: absolute;
+    color: ${theme.Colors.text};
+    bottom: 8px;
+    right: 8px;
+    cursor: pointer;
+  }
+`
+
+export const TotalContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 24px 0 16px;
+`
+
+export const Title = styled.h3`
+  font-size: 18px;
+  font-weight: 900;
+  color: ${theme.Colors.text};
+  margin-bottom: 16px;
+`
+
+export const SubTitle = styled.h3`
+  font-size: 16px;
+  font-weight: 700;
+  color: ${theme.Colors.primary};
+  margin-bottom: 16px;
+`
+
+export const Price = styled.p`
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 22px;
+  color: ${theme.Colors.text};
+`
+
+export const Text = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${theme.Colors.primary};
+`
+
+export const FormContainer = styled.form`
+  margin-bottom: 8px;
+
+  ${ButtonContainer} {
+    margin-top: 24px;
+  }
+`
+
+export const InputGroup = styled.div<InputGroupProps>`
+  flex: auto;
+  max-width: ${(props) => props.maxWidth || 'auto'};
+
+  label {
+    font-size: 14px;
+    font-weight: 700;
+    color: ${theme.Colors.primary};
+    display: block;
+    margin: 8px 0;
+  }
+
+  input {
+    width: 100%;
+    background-color: ${theme.Colors.primary};
+    height: 32px;
+    padding: 8px;
+    border: 1px solid ${theme.Colors.primary};
+
+    &.error {
+      border: 1px solid red;
+    }
+  }
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  gap: 34px;
+`
+
+export const TextContainer = styled.div`
+  margin: 16px 0 24px;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 22px;
+    color: ${theme.Colors.primary};
+  }
+`
